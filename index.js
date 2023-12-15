351c40dd79f588543611a7491792a415e3ad9be5
Brenden Hartman <brendenhart22@gmail.com>
2023-12-15 12:36:09 -0600

    I love javascript

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return "This one is on me!";
+  }
+  else if(feet > 400 && feet <= 2000){
+    return "That will be twenty bucks.";
+  }
+  else if(feet > 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   let result = "";
+   city === "NYC" ? (result = "Ok, sounds good.") : (result = "No go."); 
+   return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous": 
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default: 
+      return "Bye.";
+      break; 
+  }
 }
